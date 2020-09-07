a68e2f25571d9bb4dc68a5be9975cdd048c7c6b9
Kyle <kylehasti@gamil.com>
2020-09-07 14:42:45 -0400

    finished

--- mimic.py
+++ mimic.py
@@ -20,7 +20,7 @@ You can try adding in line breaks around 70 columns so the output looks
 better.
 """
 
-__author__ = "???"
+__author__ = "Kyle Hastings, recieved help from John wilkinson"
 
 
 import random
@@ -28,38 +28,66 @@ import sys
 
 
 def create_mimic_dict(filename):
-    """Returns a dict mapping each word to a list of words which follow it.
-    For example:
-        Input: "I am a software developer, and I don't care who knows"
-        Output:
-            {
-                "" : ["I"],
-                "I" : ["am", "don't"],
-                "am": ["a"],
-                "a": ["software"],
-                "software" : ["developer,"],
-                "developer," : ["and"],
-                "and" : ["I"],
-                "don't" : ["care"],
-                "care" : ["who"],
-                "who" : ["knows"]
-            }
-    """
+    mimic_dict = {"" : []}
+    with open(filename) as f:
+        text  = f.read()
+        print(text)
+        words = text.split()
+        mimic_dict[""].append(words[0])
+        for num, word in enumerate(words):
+            if num != len(words) - 1:
+                if word in mimic_dict:
+                    mimic_dict[word].append(words[num + 1])
+                else:
+                    mimic_dict[word] = [words[num + 1]]
+
+    print(mimic_dict)
+    return mimic_dict
+
+    # """Returns a dict mapping each word to a list of words which follow it.
+    # For example:
+    #     Input: "I am a software developer, and I don't care who knows"
+    #     Output:
+    #         {
+    #             "" : ["I"],
+    #             "I" : ["am", "don't"],
+    #             "am": ["a"],
+    #             "a": ["software"],
+    #             "software" : ["developer,"],
+    #             "developer," : ["and"],
+    #             "and" : ["I"],
+    #             "don't" : ["care"],
+    #             "care" : ["who"],
+    #             "who" : ["knows"]
+    #         }
+    # """
     # +++your code here+++
-    pass
 
 
 def print_mimic_random(mimic_dict, num_words):
-    """Given a previously created mimic_dict and num_words,
-    prints random words from mimic_dict as follows:
-        - Use a start_word of '' (empty string)
-        - Print the start_word
-        - Look up the start_word in your mimic_dict and get its next-list
-        - Randomly select a new word from the next-list
-        - Repeat this process num_words times
-    """
+    result_word = ""
+    start_word = ""
+    i = 0
+    while i < num_words:
+        if start_word in mimic_dict:
+            random_word = random.choice(mimic_dict[start_word]) 
+            result_word += " " + random_word + " "
+            start_word = random_word
+        else:
+            start_word = ""
+        i += 1
+    print(result_word)
+    result_word = result_word.strip("\n")
+    return result_word
+    # """Given a previously created mimic_dict and num_words,
+    # prints random words from mimic_dict as follows:
+    #     - Use a start_word of '' (empty string)
+    #     - Print the start_word
+    #     - Look up the start_word in your mimic_dict and get its next-list
+    #     - Randomly select a new word from the next-list
+    #     - Repeat this process num_words times
+    # """
     # +++your code here+++
-    pass
 
 
 def main(args):
